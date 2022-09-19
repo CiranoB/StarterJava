f696efe3b0c1978eeaf31d54ecf1f69bc5b4aedc
CiranoB <ciranobelardony@gmail.com>
2022-09-19 17:19:30 -0300

    Criação da Home

--- Front-end/src/pages/home/Home.tsx
+++ Front-end/src/pages/home/Home.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react';
 import { Typography, Grid, Button } from '@material-ui/core';
 import { Box } from '@mui/material'
 import './Home.css';
@@ -7,25 +6,52 @@ import './Home.css';
 function Home() {
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+            <Grid container direction="row" justifyContent="space-evenly" alignItems="center" className='caixa'>
                 <Grid alignItems="center" item xs={6}>
-                    <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    <Box alignItems="center">
+                        <Typography variant="h2" gutterBottom color="textPrimary"  align="center" className='titulo'>NutritionAPI</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='subtitulo'>
+                          <div>Nunca foi tão fácil seguir uma dieta</div>
+                           </Typography>
+                           <Typography variant="h5" gutterBottom color="textPrimary" component="h1" align="center" className='titulo'>
+                           <Button color="inherit" variant="contained" className='botao'>CADASTRE-SE</Button> 
+                           </Typography>
+                           
                     </Box>
-                    <Box display="flex" justifyContent="center">
-                        <Box marginRight={1}>
-                
-                        </Box>
-                        <Button variant="outlined" className='botao'>Ver Postagens</Button>
-                    </Box>
+                    
                 </Grid>
-                <Grid item xs={6} >
-                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                <Grid container xs={6} justifyContent="space-evenly" >
+                    
+                    <Box alignItems="center">
+                    <img src="https://i.ytimg.com/vi/KzfsQdZRsiE/maxresdefault.jpg" className='image'/>
+                    </Box>
                 </Grid>
                 <Grid xs={12} className='postagens'>
             
                 </Grid>
+            </Grid>
+            <Grid container direction="row" justifyContent="space-evenly" alignItems="center" className='howitworks'>
+               
+                    <Box >
+                        <Typography variant="h2" gutterBottom color="textPrimary"  align="center" className='titulo2'>Afinal, como funciona?</Typography>
+                           
+                    </Box>
+                
+            </Grid>
+
+            <Grid container direction="row" justifyContent="space-evenly" alignItems="center" className='caixa'>
+                <Grid xs={2}  alignItems="center">
+                    <Box >
+                        <Typography variant="h5" gutterBottom color="textPrimary"  align="center" className='subtitulo'>1º</Typography>
+                    </Box>
+                </Grid>
+                <Grid xs={10} alignItems="center" >
+                    <Box >
+                        <Typography variant="h6" gutterBottom color="textPrimary"  align="center" className='subtitulo'>Você se cadastra, informando dados
+                        básicos físicos como altura, peso, gênero, nome, etc e seu objetivo (aumentar, diminuir ou manter o peso)</Typography>
+                    </Box>
+                </Grid>
+                
             </Grid>
         </>
     );
