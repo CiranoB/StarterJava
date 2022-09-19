cf64c294db5d475a9ce85daeda77108d2ac99e53
CiranoB <ciranobelardony@gmail.com>
2022-09-19 09:36:42 -0300

    Gerando o front-end

--- Back-end/nutritionapi/src/main/java/com/gft/starter/nutritionapi/NutritionApiApplication.java
+++ Back-end/nutritionapi/src/main/java/com/gft/starter/nutritionapi/NutritionApiApplication.java
@@ -1,7 +1,9 @@
 package com.gft.starter.nutritionapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class NutritionApiApplication {
 
 	public static void main(String[] args) {
