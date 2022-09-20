357b34ac09d1dee95a48edf2c9e5c54a06db7531
Neres <joaovneres@outlook.com>
2022-09-20 13:21:42 -0300

    Criação e estilização da Footer

--- Front-end/src/components/static/footer/Footer.tsx
+++ Front-end/src/components/static/footer/Footer.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
-import { Typography, Grid } from '@material-ui/core';
+import { Typography, Grid, Box } from '@material-ui/core';
 import './Footer.css'
 
 function Footer() {
-    return (  
+    return (
         <Grid className='containerFooter'>
-            <Grid className='boxFooter'></Grid>
-            <Grid className='boxFooter'></Grid>
-            <Grid className='boxFooter'></Grid>
+            <Grid className='fFooter'>
+                <Box>
+                    <Typography
+                        variant='caption'
+                        className='bolder'>
+                        © 2022 Copyright:
+                    </Typography>
+                    <br />
+                    <Typography
+                        variant='caption'
+                        className='bolder'>
+                        <a href='https://github.com/CiranoB' className='linkNone'>Cirano Belardony</a>, <a href='https://github.com/joaovneres' className='linkNone'>João Victor Neres</a>, <a href='https://github.com/JVitor25' className='linkNone'>João Vitor Lima.</a>
+                    </Typography>
+                </Box>
+            </Grid>
         </Grid>
-  );
+    );
 }
 
 export default Footer;
