861d5314725edfce5a160f58a96085c4d73ac5b6
Joao Vitor Lima <joaovitorsocial25@gmail.com>
2022-09-23 12:19:38 -0300

    RegisterPay

--- Front-end/src/components/pay/deletePay/DeletePay.tsx
+++ Front-end/src/components/pay/deletePay/DeletePay.tsx
@@ -1,12 +1,14 @@
-import { Grid, Box, Card, CardContent, Typography, Divider, CardActions, Button } from "@mui/material";
+import { Grid, Box, Card, CardContent, Typography, Divider, CardActions, Button, IconButton } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
+import CreateIcon from '@mui/icons-material/Create';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Link, useNavigate, useParams } from "react-router-dom";
 import Pay from "../../../models/Pay";
 import { deletePay, getIdPay } from "../../../services/Service";
 import { TokenState } from "../../../store/tokens/tokensReducer";
 import { toast } from "react-toastify";
-
+import "./DeletePay.css";
 
 
 export default function DeletePay() {
@@ -58,39 +60,70 @@ export default function DeletePay() {
         <Grid container xs={12}>
             <Grid xs={4}></Grid>
             <Grid xs={4}>
-                <Box m={2}>
-                    <Card variant="outlined" className="cardDeletarPostagem">
-                        <CardContent>
+                <Box m={2} className="boxDeletePay">
+                    <Grid container direction="column">
+                        <Grid item>
                             <Box justifyContent="center">
                                 <Typography variant="h5" color="textSecondary" gutterBottom>
-                                    Deseja deletar a Postagem:
-                                </Typography>
-                                <Typography variant="h6" color="textSecondary" >
-                                    oi
-                                    {pay?.valuePay}
+                                    Deseja apagar a Fatura:
                                 </Typography>
                             </Box>
-                        </CardContent>
-                        <Box display="flex" flexDirection="column" sx={{ width: "100%" }} alignItems="center">
-                            <Box sx={{ width: "95%" }}>
-                                <Divider />
+                        </Grid>
+                        <Grid item>
+                            <Box marginTop={2}>
+                                <Grid
+                                    sx={{ background: "#DCE3E1", borderRadius: '10px' }}
+                                    p={2}
+                                    container
+                                    spacing={1}
+                                    direction="column">
+                                    <Grid item
+                                        container
+                                        direction="row">
+                                        <Grid xs={3}>
+                                            <Box>Valor: R${pay?.valuePay}</Box>
+                                        </Grid>
+                                        <Grid xs={7}>
+                                            <Box>Data do vencimento: {pay?.datePay}</Box>
+                                        </Grid>
+                                        <Grid xs={2}>
+                                            <Box>
+                                                <Link to={`/formularioPay/${pay?.uuidPay}`} className="text-decorator-none" >
+                                                    <IconButton>
+                                                        <CreateIcon fontSize="small" />
+                                                    </IconButton>
+                                                </Link>
+                                            </Box>
+                                        </Grid>
+                                    </Grid>
+                                    <Box sx={{ backgroundColor: "#DEBA7C", padding: "0.5px" }}><Divider /></Box>
+                                    <Grid item marginTop={0.5}>
+                                        <Box>Pagamento efetuado no dia: {pay?.dueDatePay} </Box>
+                                    </Grid>
+                                    <Grid item>
+                                        <Box sx={{ marginTop: "20px" }}> Última edição feita por: {pay?.bookkeeper?.namePerson} - Registro: {pay?.bookkeeper?.registerBookkeeper}</Box>
+                                    </Grid>
+                                </Grid>
                             </Box>
-                        </Box>
-                        <CardActions>
-                            <Box mb={0.5} mt={0.5} className="ActionsDeletarPostagem" >
-                                <Box>
+                            <Box display="flex" flexDirection="column" sx={{ width: "100%" }} alignItems="center">
+                                <Box sx={{ width: "95%" }} marginTop={2}>
+                                    <Divider />
+                                </Box>
+                            </Box>
+                            <Box className="botaoDeletePay" >
+                                <Box padding={1}>
                                     <Button onClick={nao} variant="contained" size='large' color="error">
                                         Não
                                     </Button>
                                 </Box>
-                                <Box mx={2}>
-                                    <Button onClick={sim} variant="contained" className="marginLeft" size='large' color="success">
+                                <Box padding={1}>
+                                    <Button onClick={sim} variant="contained" size='large' color="success">
                                         Sim
                                     </Button>
                                 </Box>
                             </Box>
-                        </CardActions>
-                    </Card>
+                        </Grid>
+                    </Grid>
                 </Box>
             </Grid>
             <Grid xs={4}></Grid>
--- Front-end/src/components/pay/registerPay/RegisterPay.tsx
+++ Front-end/src/components/pay/registerPay/RegisterPay.tsx
@@ -8,7 +8,7 @@ import { getAllPay, getAllUser, getIdPay, getIdUser, registerPay, updatePay } fr
 import User from "../../../models/User";
 import Bookkeeper from "../../../models/Bookkeeper";
 import { useNavigate, useParams } from "react-router-dom";
-import { Box, Button, Container, FormControl, FormHelperText, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material";
+import { Box, Button, Container, FormControl, FormHelperText, Grid, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material";
 
 function RegisterPay() {
     let navigate = useNavigate();
@@ -72,14 +72,20 @@ function RegisterPay() {
     }, [uuidPay]);
 
     async function findByPayUuid(uuidPay: string) {
-        getIdPay(`/bookkeeper/pay/find/${uuidPay}`, setPay, {
+        await getIdPay(`/bookkeeper/pay/find/${uuidPay}`, setPay, {
+            headers: {
+                'Authorization': token 
+            }
+        })
+    }
+
+    async function findByUserUuid() {
+        await getIdUser(`/nutritionist/user/find/${pay?.user?.uuidPerson}`, setUser, {
             headers: {
                 'Authorization': token
             }
-        })
-        console.log(pay)
+        })   
     }
-
     async function getUsers() {
         await getAllUser("/nutritionist/user/all", setUsers, {
             headers: {
@@ -140,85 +146,67 @@ function RegisterPay() {
     }
 
     var componentsRegisterPay;
-    
+    var tituloRegisterPay;
     if (uuidPay !== undefined) {
-        componentsRegisterPay = <Container maxWidth="sm" className="formularioPay">
-            <form onSubmit={onSubmit}>
-                <Typography variant="h3" color="textSecondary" component="h1" align="center">
-                    Formulário de cadastro de Faturas
-                </Typography>
-                <TextField value={pay.datePay} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedPay(e)} id="datePay" label="Data de vencimento da Fatura" name="datePay" variant="outlined" margin="normal" fullWidth />
-                <TextField value={pay.valuePay} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedPay(e)} id="valuePay" label="Valor da Fatura" name="valuePay" variant="outlined" margin="normal" fullWidth />
-                <FormControl fullWidth variant="standard">
-                    <InputLabel sx={{ fontSize: 14 }} id="demo-simple-select-helper-label">Usuarios:</InputLabel>
-                    <Select>
-                        Teste
-                    </Select>
-                    <FormHelperText sx={{ fontSize: 14 }}> Selecione o Usuário</FormHelperText>
-                    <TextField value={pay.dueDatePay} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedPay(e)} id="dueDatePay" label="A Fatura foi paga no dia:" name="dueDatePay" variant="outlined" margin="normal" fullWidth />
-                    <Box className="botaoRegisterPay">
-                        <Box padding={1}>
-                            <Button variant="contained" color="error" onClick={back}>
-                                Voltar
-                            </Button>
-                        </Box>
-                        <Box padding={1}>
-                            <Button type="submit" variant="contained" color="success">
-                                Finalizar
-                            </Button>
-                        </Box>
-                    </Box>
-                </FormControl>
-            </form>
-        </Container>
+
+        tituloRegisterPay = <Typography variant="h4" color="textSecondary" component="h1" align="center">
+            Editando fatura:
+        </Typography>
     } else {
-        componentsRegisterPay = <Container maxWidth="sm" className="formularioPay">
-            <form onSubmit={onSubmit}>
-                <Typography variant="h3" color="textSecondary" component="h1" align="center">
-                    Formulário de cadastro de Faturas
-                </Typography>
-                <TextField value={pay.datePay} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedPay(e)} id="datePay" label="Data de vencimento da Fatura" name="datePay" variant="outlined" margin="normal" fullWidth />
-                <TextField value={pay.valuePay} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedPay(e)} id="valuePay" label="Valor da Fatura" name="valuePay" variant="outlined" margin="normal" fullWidth />
-                <FormControl fullWidth variant="standard">
-                    <InputLabel sx={{ fontSize: 14 }} id="demo-simple-select-helper-label">Usuarios:</InputLabel>
-                    <Select
-                        sx={{ backgroundColor: "white", borderRadius: '5px', marginBottom: "1px" }}
-                        labelId="demo-simple-select-helper-label"
-                        id="demo-simple-select-helper"
-                        onChange={(e) => getIdUser(`/nutritionist/user/find/${e.target.value}`, setUser, {
-                            headers: {
-                                'Authorization': token
-                            }
-                        })}>
-                        {users.map(user => (
-                            <MenuItem value={user.uuidPerson}>
-                                <Box display="flex" sx={{ maxWidth: 340, whiteSpace: "normal", textAlign: "justify", fontWeight: 500 }} >
-                                    {user.namePerson}
-                                </Box>
-                            </MenuItem>
-                        ))}
-                    </Select>
-                    <FormHelperText sx={{ fontSize: 14 }}> Selecione o Usuário</FormHelperText>
-                    <TextField value={pay.dueDatePay} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedPay(e)} id="dueDatePay" label="A Fatura foi paga no dia:" name="dueDatePay" variant="outlined" margin="normal" fullWidth />
-                    <Box className="botaoRegisterPay">
-                        <Box padding={1}>
-                            <Button variant="contained" color="error" onClick={back}>
-                                Voltar
-                            </Button>
-                        </Box>
-                        <Box padding={1}>
-                            <Button type="submit" variant="contained" color="success">
-                                Finalizar
-                            </Button>
-                        </Box>
-                    </Box>
-                </FormControl>
-            </form>
-        </Container>
+        tituloRegisterPay = <Typography variant="h4" color="textSecondary" component="h1" align="center">
+            Formulário de cadastro de Faturas
+        </Typography>
     }
+
     return (
         <>
-            {componentsRegisterPay}
+            <Grid xs={12}>
+                <Box p={1}>
+                    <Container maxWidth="sm" className="formularioPay">
+                        <form onSubmit={onSubmit}>
+                            <Box m={1}>
+                                {tituloRegisterPay}
+                            </Box>
+                            <TextField value={pay.datePay} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedPay(e)} id="datePay" label="Data de vencimento da Fatura" name="datePay" variant="outlined" margin="normal" fullWidth />
+                            <TextField value={pay.valuePay} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedPay(e)} id="valuePay" label="Valor da Fatura" name="valuePay" variant="outlined" margin="normal" fullWidth />
+                            <FormControl fullWidth variant="standard">
+                                <InputLabel sx={{ fontSize: 14 }} id="demo-simple-select-helper-label">Usuarios:</InputLabel>
+                                <Select
+                                    sx={{ backgroundColor: "white", borderRadius: '5px', marginBottom: "1px" }}
+                                    labelId="demo-simple-select-helper-label"
+                                    id="demo-simple-select-helper"
+                                    onChange={(e) => getIdUser(`/nutritionist/user/find/${e.target.value}`, setUser, {
+                                        headers: {
+                                            'Authorization': token
+                                        }
+                                    })}>
+                                    {users.map(user => (
+                                        <MenuItem value={user.uuidPerson}>
+                                            <Box display="flex" sx={{ maxWidth: 340, whiteSpace: "normal", textAlign: "justify", fontWeight: 500 }} >
+                                                {user.namePerson}
+                                            </Box>
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                            <FormHelperText sx={{ fontSize: 14 }}> Selecione o Usuário</FormHelperText>
+                            <TextField value={pay.dueDatePay} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedPay(e)} id="dueDatePay" label="A Fatura foi paga no dia:" name="dueDatePay" variant="outlined" margin="normal" fullWidth />
+                            <Box className="botaoRegisterPay">
+                                <Box padding={1}>
+                                    <Button variant="contained" color="error" onClick={back}>
+                                        Voltar
+                                    </Button>
+                                </Box>
+                                <Box padding={1}>
+                                    <Button type="submit" variant="contained" color="success">
+                                        Finalizar
+                                    </Button>
+                                </Box>
+                            </Box>
+                        </form>
+                    </Container>
+                </Box>
+            </Grid>
         </>
     )
 }
